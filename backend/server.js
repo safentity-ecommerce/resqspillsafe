dd44813fb342b3765f5b9697dd664085f7ec1a4d
Prateek <prateekkukreja@outlook.com>
2026-06-14 18:20:57 +0530

    connected frontend with deployed backend

--- backend/server.js
+++ backend/server.js
@@ -51,7 +51,7 @@ app.post("/send-email", async (req, res) => {
   }
 })
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
